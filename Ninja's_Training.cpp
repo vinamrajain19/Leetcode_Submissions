bb0be798c444fe57464354d3e80bd603f2ad4e1d
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-04-17 15:56:14 +0530

    Create Ninja's_Training.cpp

---
+++ Ninja's_Training.cpp
@@ -0,0 +1,61 @@
+/* Ninja is planing this ‘N’ days-long training schedule. Each day, he can perform any one of these three activities. (Running, Fighting Practice or Learning New Moves). Each activity has some merit points on each day. As Ninja has to improve all his skills, he can’t do the same activity in two consecutive days. Can you help Ninja find out the maximum merit points Ninja can earn?
+You are given a 2D array of size N*3 ‘POINTS’ with the points corresponding to each day and activity. Your task is to calculate the maximum number of merit points that Ninja can earn. */
+
+
+// Ques Link
+//https://www.codingninjas.com/codestudio/problems/ninja-s-training_3621003?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+/* int ninjaTraining(int n, vector < vector < int > > & points) {
+// S.C. = O(N*4)
+  vector < vector < int > > dp(n, vector < int > (4, 0));
+
+  dp[0][0] = max(points[0][1], points[0][2]);
+  dp[0][1] = max(points[0][0], points[0][2]);
+  dp[0][2] = max(points[0][0], points[0][1]);
+  dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+  for (int day = 1; day < n; day++) {
+    for (int last = 0; last < 4; last++) {
+      dp[day][last] = 0;
+      for (int task = 0; task <= 2; task++) {
+        if (task != last) {
+          int activity = points[day][task] + dp[day - 1][task];
+          dp[day][last] = max(dp[day][last], activity);
+        }
+      }
+    }
+
+  }
+
+  return dp[n - 1][3];
+} */
+
+// Space Optimised
+// S.C = O(4)
+
+int ninjaTraining(int n, vector < vector < int > > & points) {
+
+  vector < int > prev(4, 0);
+
+  prev[0] = max(points[0][1], points[0][2]);
+  prev[1] = max(points[0][0], points[0][2]);
+  prev[2] = max(points[0][0], points[0][1]);
+  prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+  for (int day = 1; day < n; day++) {
+
+    vector < int > temp(4, 0);
+    for (int last = 0; last < 4; last++) {
+      temp[last] = 0;
+      for (int task = 0; task <= 2; task++) {
+        if (task != last) {
+          temp[last] = max(temp[last], points[day][task] + prev[task]);
+        }
+      }
+    }
+
+    prev = temp;
+
+  }
+
+  return prev[3];
+}
