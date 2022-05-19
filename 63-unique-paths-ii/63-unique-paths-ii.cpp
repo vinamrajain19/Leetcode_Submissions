a9868025f7c8a1fe3133e7989404bbe01b44f549
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-20 00:33:12 +0530

    Time: 9 ms (17.13%), Space: 7.7 MB (60.39%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& g) {
-        vector<vector<int>> dp(g.size(),vector<int>(g[0].size(),-1));
-        return helper(g,g.size(),g[0].size(),0,0,dp);
+        int m = g.size();
+        int n = g[0].size();
+        
+        if(g[0][0] == 1) return 0;
+        vector<int> prev(n,0);
+        
+        for(int i = 0;i<m;i++){
+            
+            vector<int> temp(n,0);
+            
+            for(int j = 0;j<n;j++){
+                
+                if(i == 0 and j == 0) temp[j] = 1;
+                else{
+                    if(g[i][j] == 1) temp[j] = 0;
+                    else{
+                        int u = 0;
+                        int l = 0;
+                        if(i>0)
+                            u = prev[j];
+                        if(j>0)
+                            l = temp[j-1];
+                        
+                        temp[j] = u + l;
+                    }
+                }
+            }
+            prev = temp;
+        }
+        return prev[n-1];
     }
     
-    int helper(vector<vector<int>>& g,int m,int n,int i,int j,vector<vector<int>> &dp){
-        if(i == m-1 and j == n-1 and g[i][j] == 0) return 1;
-        
-        if(i>=m or j>=n) return 0;
-        
-        if(g[i][j] == 1) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        return dp[i][j] = helper(g,m,n,i+1,j,dp)+helper(g,m,n,i,j+1,dp);
-        
-    
-    }
+
 };
