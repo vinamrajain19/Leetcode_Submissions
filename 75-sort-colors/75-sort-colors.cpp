93d21459d27bae4fbae61a0b46c52c8cddaf575e
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-21 00:29:21 +0530

    Time: 3 ms (58.33%), Space: 8.3 MB (71.29%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int f =0,s=0,z=0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == 0) z++;
+            else if(nums[i] == 1) f++;
+            else s++;
+        }
+        nums.clear();
+        for(int i = 0;i<z;i++){
+            nums.push_back(0);
+        }
+        for(int i = 0;i<f;i++){
+            nums.push_back(1);
+        }
+        for(int i = 0;i<s;i++){
+            nums.push_back(2);
+        }
+        
+    }
+};
