00bba89894eacac93f1223a967362da7fede099f
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-20 15:31:20 +0530

    Time: 9 ms (45.89%), Space: 8.9 MB (45.12%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& t) {
-        vector<vector<int>> dp(t.size(),vector<int>(t.size(),-1));
-        return help(0,0,t.size(),t.size(),t,dp);
+       // vector<vector<int>> dp(t.size(),vector<int>(t.size(),-1));
+        
+        int n = t.size();
+        
+        vector<int> last(n,0);
+        
+        for(int j = 0;j<n;j++){
+            last[j] = t[n-1][j];
+        }
+        
+        for(int i = n-2;i>=0;i--){
+            vector<int> temp(n,0);
+            for(int j = i;j>=0;j--){
+                int d = t[i][j] + last[j];
+                int dia = t[i][j] + last[j+1];
+                
+                temp[j] = min(d,dia);
+            }
+            last = temp;
+        }
+        
+        return last[0];
+        
     }
     
-    int help(int i,int j,int n,int m,vector<vector<int>>& t,vector<vector<int>> &dp){
-        if(i == m-1) return dp[i][j] = t[i][j];
-        
-        if(i>=m or j>=n) return 1e9;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int d = t[i][j] + help(i+1,j,m,n,t,dp);
-        int dia = t[i][j] + help(i+1,j+1,m,n,t,dp);
-        
-        return dp[i][j] = min(d,dia);
-    }
 };
