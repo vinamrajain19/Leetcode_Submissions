81d05610186093ec286cc3a2cc33caf8ad4b6361
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-20 12:45:10 +0530

    Time: 22 ms (11.10%), Space: 10.1 MB (48.87%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return help(grid,grid.size()-1,grid[0].size()-1,dp);
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else{
+                    int u = grid[i][j];
+                    if(i>0){
+                        u += dp[i-1][j];
+                    }
+                    else u = 1e9;
+                    
+                    int l = grid[i][j];
+                    if(j>0){
+                        l += dp[i][j-1];
+                    }
+                    else l = 1e9;
+                    
+                    dp[i][j] = min(u,l);
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
-    int help(vector<vector<int>>& grid,int i,int j,vector<vector<int>> &dp){
-        
-        if(i<0 or j<0) return 1e9;
-        
-        if(i == 0 and j == 0) return dp[i][j] = grid[0][0];
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int u = grid[i][j] + help(grid,i-1,j,dp);
-        int l = grid[i][j] + help(grid,i,j-1,dp);
-        
-        return dp[i][j] = min(u,l);
-    }
+  
 };
