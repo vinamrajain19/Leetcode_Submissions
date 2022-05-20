339ab973c5b76c33fe4ad93dda7048b13e554d7e
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-20 21:28:08 +0530

    Time: 16 ms (71.97%), Space: 10.3 MB (44.11%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -2,32 +2,36 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+      //  vector<vector<int>> dp(n,vector<int>(n,-1));
         // call for every element in last row and find min of all because it is a variable starting pt and variable ending pt problem
+        
+        vector<int> prev(n,0);
         
         //initialsation
         for(int i = 0;i<n;i++){
-            dp[0][i] = matrix[0][i];
+            prev[i] = matrix[0][i];
         }
         for(int i = 1;i<n;i++){
+            vector<int> temp(n,0);
             for(int j = 0;j<n;j++){
-                int u = matrix[i][j] + dp[i-1][j];
+                int u = matrix[i][j] + prev[j];
                 
                 int ld = matrix[i][j];
-                if(j-1 >= 0) ld += dp[i-1][j-1];
+                if(j-1 >= 0) ld += prev[j-1];
                 else ld = 1e9;
                 
                 int rd = matrix[i][j];
-                if(j+1 < n) rd += dp[i-1][j+1];
+                if(j+1 < n) rd += prev[j+1];
                 else rd = 1e9;
                 
-                dp[i][j] = min(u,min(ld,rd));
+                temp[j] = min(u,min(ld,rd));
             }
+            prev = temp;
         }
         
         int ans = 1e9;
         for(int i = 0;i<n;i++){
-            ans = min(ans,dp[n-1][i]);
+            ans = min(ans,prev[i]);
         }
         
         return ans;
