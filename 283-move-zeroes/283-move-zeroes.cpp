d6f91109cf2fb7baaa42d1d967f428dd611063af
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-21 21:46:02 +0530

    Time: 23 ms (85.30%), Space: 19.3 MB (58.52%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-        int cnt2 = 0;
+       /* int cnt2 = 0;
         for(int i = 0;i<nums.size();i++){
             if(nums[i] != 0){
                 nums[cnt2] = nums[i];
@@ -11,6 +11,18 @@ public:
         }
         for(int i = cnt2;i<nums.size();i++){
             nums[i] = 0;
+        } */
+        
+        // two pointer method
+        
+        int l = 0;
+        int h = 0;
+        while(h<nums.size()){
+            if(nums[h] != 0){
+                swap(nums[h],nums[l]);
+                l++;
+            }
+            h++;
         }
     }
 };
