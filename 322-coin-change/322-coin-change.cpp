491049a2953d168d92db3cfaa935b1825ec345f9
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-23 14:39:14 +0530

    Time: 130 ms (44.49%), Space: 40.8 MB (22.41%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-    int help(int idx,vector<int>& coins, int amt,vector<vector<int>> &dp){
-        
-        //base case
-        
-        if(amt == 0) return 0;
-        if(idx == 0){
-            if(amt % coins[0] == 0) return (amt/coins[0]);
-            return 1e9;
-        }
-        
-        if(dp[idx][amt] != -1) return dp[idx][amt];
-        
-        int nottake = help(idx-1,coins,amt,dp);
-        int take = 1e9;
-        if(coins[idx] <= amt){
-            take =  1 + help(idx,coins,amt-coins[idx],dp);
-        }
-        
-        return dp[idx][amt] = min(nottake,take);
-    }
     
     int coinChange(vector<int>& coins, int amt) {
         
