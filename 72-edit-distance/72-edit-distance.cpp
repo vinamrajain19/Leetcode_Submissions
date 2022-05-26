cd7cf011493e122b5e2cc10ac000b2b435955174
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-26 20:00:08 +0530

    Time: 34 ms (26.07%), Space: 6.5 MB (92.88%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int help(string &s1, string &s2,int i,int j,vector<vector<int>> &dp) {
+ /*   int help(string &s1, string &s2,int i,int j,vector<vector<int>> &dp) {
        
         if(j<0) return i+1;
         if(i<0) return j+1;
@@ -12,12 +12,33 @@ public:
         //insert -> f(i,j-1)   delete -> f(i-1,j)     replace -> f(i-1,j-1)
         
         return dp[i][j] = 1 + min(help(s1,s2,i-1,j-1,dp) ,min(help(s1,s2,i-1,j,dp) , help(s1,s2,i,j-1,dp)));
-    }
+    } */
     
     int minDistance(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return help(s1,s2,n-1,m-1,dp);
+      //  vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        vector<int> prev(m+1,0),temp(m+1,0);
+    
+        
+        for(int j = 0;j<m+1;j++){
+            prev[j] = j;
+        }
+        
+        for(int i = 1;i<n+1;i++){
+            
+            temp[0] = i;
+            
+            for(int j = 1;j<m+1;j++){
+                if(s1[i-1] == s2[j-1]) temp[j] = prev[j-1];
+                else{
+                    temp[j] = 1 + min({prev[j-1] , prev[j] ,temp[j-1]});
+                }
+            }
+            prev = temp;
+        }
+        
+        return prev[m];
     }
 };
