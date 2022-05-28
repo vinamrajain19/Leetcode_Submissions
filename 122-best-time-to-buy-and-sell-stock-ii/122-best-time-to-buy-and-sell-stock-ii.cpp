8a5d7ed794c8b503360c3cf3748bcfaae530b363
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-05-28 16:19:01 +0530

    Time: 20 ms (8.96%), Space: 13.1 MB (53.64%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -23,14 +23,9 @@ public:
        vector<int> prev(2,0),curr(2,0);
         
         for(int i = n-1;i>=0;i--){
-            for(int j = 0;j<=1;j++){
-                if(j){
-                    curr[j] = max(-p[i] + prev[0],prev[1]);
-                }
-                else{
-                    curr[j] =  max(p[i] + prev[1],prev[0]);
-                }
-            }
+            
+            curr[1] = max(-p[i] + prev[0],prev[1]);
+            curr[0] =  max(p[i] + prev[1],prev[0]);
             prev = curr;
         }
         
