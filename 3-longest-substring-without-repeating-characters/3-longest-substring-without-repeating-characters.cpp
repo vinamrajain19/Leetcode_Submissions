df48161941f9006382dd5481270437559614ae66
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-06-11 01:00:20 +0530

    Time: 19 ms (65.87%), Space: 9.1 MB (36.39%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int i = 0;
+        int j = 0;
+        
+        int ans = 0;
+        unordered_map<char,int> m;
+        
+        while(j<s.size()){
+            m[s[j]]++;
+            if(m.size() == j-i+1){
+                ans = max(ans,j-i+1);
+                j++;
+            }
+            else if(m.size() < j-i+1){
+                while(m.size() < j-i+1){
+                    m[s[i]]--;
+                    if(m[s[i]] == 0) m.erase(s[i]);
+                    i++;
+                }
+                j++;
+            }
+        }
+        return ans;
+    }
+};
