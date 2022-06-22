791c5e7f684984c9954f9e43ba8793f2e95f21b5
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-06-23 03:19:16 +0530

    Time: 50 ms (20.76%), Space: 13.8 MB (63.34%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> par;
+    int find(int u){
+        if(u == par[u]){
+            return u;
+        }
+        return par[u] = find(par[u]);
+    }
+    
+    void unnion(int u,int v){
+        u = find(u);
+        v= find(v);
+        if(u == v) return;
+        par[v] = u;
+    }
+    
+    int findCircleNum(vector<vector<int>>& is) {
+        par.resize(is.size());
+        for(int i = 0;i<is.size();i++){
+            par[i] = i;
+        }
+        
+        for(int i = 0;i<is.size();i++){
+            for(int j = 0;j<is[0].size();j++){
+                if(is[i][j] == 1){
+                    unnion(i,j);
+                }
+            }
+        }
+        
+        set<int> s;
+        int c = 0;
+        for(int i = 0;i<par.size();i++){
+            if(par[i] == i) c++;
+        }
+        return c;
+        
+    }
+};
