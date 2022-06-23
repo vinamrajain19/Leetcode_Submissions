015a1c717817c650274f19c3b0d7f2bbd742f320
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-06-23 16:00:59 +0530

    Time: 306 ms (38.57%), Space: 14.2 MB (89.98%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -2,38 +2,43 @@ class Solution {
 public:
     // use union find -> when we get row or col equal -> union those -> and find -> how many parents are there;
     
-    vector<int> par;
-    
+   vector<int> par;
     int find(int u){
         if(u == par[u]) return u;
         return par[u] = find(par[u]);
     }
-    
-    bool unnion(int u,int v){
+    void unnion(int u,int v){
         u = find(u);
         v = find(v);
         if(u == v){
-            return false;
+            return;
         }
         par[v] = u;
-        return true;
     }
     
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        par.resize(n);
+        par.resize(n,0);
         for(int i = 0;i<n;i++){
             par[i] = i;
         }
-        int ans = 0;
+        
+        
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
+            for(int j = i+1;j<n;j++){
                 if(stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]){
-                    if(unnion(i,j)) ans++;  
+                    unnion(i,j); 
                 }
             }
         }
+        
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            if(i == par[i]){
+                ans++;
+            }
+        }
+        return n-ans;
        
-        return ans;
     }
 };
