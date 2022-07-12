16bf6a7f194dea10d1c83337444ed42c04a6ba78
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-07-12 14:48:30 +0530

    Time: 644 ms (10.45%), Space: 106.5 MB (17.81%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        unordered_map<int,unordered_set<int>> m;
+        for(auto it : logs){
+            m[it[0]].insert(it[1]);
+        }
+        
+        vector<int> ans(k,0);
+        for(auto it : m){
+            int x = it.second.size();
+            ans[x-1]++;
+        }
+        
+        return ans;
+    }
+};
+
