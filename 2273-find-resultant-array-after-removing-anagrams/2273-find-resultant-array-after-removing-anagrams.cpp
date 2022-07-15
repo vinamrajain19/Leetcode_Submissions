4b0b8cd582ea989aaac9d9c9684b04455471a09d
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2022-07-16 02:38:19 +0530

    Time: 10 ms (85.03%), Space: 12.3 MB (71.34%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& w) {
+        vector<string> ans;
+        ans.push_back(w[0]);
+        string t = w[0];
+        sort(t.begin(),t.end());
+        for(int i = 1;i<w.size();i++){
+            string x = w[i];
+            sort(x.begin(),x.end());
+            if(x==t){
+                continue;
+            }
+            else{
+                ans.push_back(w[i]);
+                t = w[i];
+                sort(t.begin(),t.end());
+            }
+        }
+        return ans;
+    }
+};
