6ac6a51b1316f94d6947b2afda742796bdf0b5ca
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-01-04 14:14:49 +0530

    Added solution - LeetHub

--- Wildcard string matching - GFG/wildcard-string-matching.cpp
+++ Wildcard string matching - GFG/wildcard-string-matching.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class Solution{
     public:
     
+    int dp[1001][1001];
      bool allstars(string s,int j){
         for(int k = 1;k<=j;k++){
             if(s[k-1] != '*') return false;
@@ -14,24 +15,27 @@ class Solution{
         return true;
     } 
     
+    
     bool f(string s,string p,int i1,int i2){
         
         // base case 
         if(i1 < 0 and i2 <0) return true;
         if(i1 < 0 and i2 >= 0) return false;
-        if(i2 < 0 and i1 >=0 ) return allstars(s,i1);
+        if(i2 < 0 and i1 >=0 ) return dp[i1][i2] = allstars(s,i1);
         
+        if(dp[i1][i2] != -1) return dp[i1][i2];
         
         if(s[i1] == p[i2] or s[i1] == '?'){
-            return f(s,p,i1-1,i2-1);
+            return dp[i1][i2]  = f(s,p,i1-1,i2-1);
         }
         if(s[i1] == '*'){
-            return f(s,p,i1-1,i2) or f(s,p,i1,i2-1);
+            return dp[i1][i2] = f(s,p,i1-1,i2) or f(s,p,i1,i2-1);
         }
-        return false;
+        return dp[i1][i2] = false;
     }
     bool match(string wild, string pattern)
     {
+        memset(dp,-1,sizeof(dp));
         return f(wild,pattern,wild.size()-1,pattern.size()-1);
     }
 };
