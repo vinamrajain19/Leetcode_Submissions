27bed8380f621c0f39b6c10bdd90b76d4be6b42e
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-01-06 13:12:19 +0530

    Time: 442 ms (27.64%), Space: 76.2 MB (99.83%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int ans=  0;
+        
+        int m = 0;
+        for(int i = 0;i<costs.size();i++){
+            if(costs[i] + m <= coins){
+                ans++;
+                m += costs[i];
+            }
+            else break;
+        }
+        return ans;
+    }
+};
