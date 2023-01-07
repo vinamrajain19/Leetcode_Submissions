df2df392958dcdab1f0526cdcf1852a786dc4365
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-01-07 18:32:39 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -7,36 +7,36 @@ using namespace std;
 class Solution{
     public:
     
-    
-    vector<int>  ms(int arr1[],int arr2[],int n,int m){
-        int i = 0;
-        int j = 0;
-        int k =0;
-        
-        vector<int> v(n+m,0);
-        
-        while(i < n and j < m){
-            if(arr1[i] < arr2[j]){
-                v[k++] = arr1[i++];
-            }
-            else v[k++] = arr2[j++];
-        }
-        
-        while(i < n){
-            v[k++] = arr1[i++];
-        }
-        
-        while(j < m){
-            v[k++] = arr2[j++];
-        }
-        
-        return v;
-    }
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        vector<int> ans = ms(arr1,arr2,n,m);
-        
-        return ans[k-1];
+        int i=0,j=0;
+        int z=0;
+        while(i<n && j<m)
+        {
+            if(arr1[i]<=arr2[j]) 
+            {
+                i++;z++;
+                if(z==k) return arr1[i-1];
+            }
+            else
+            {
+                j++;z++;
+                 if(z==k) return arr2[j-1];
+            }
+        }
+
+        while(i<n)
+        {
+            i++;z++;
+            if(z==k) return arr1[i-1];
+        }
+
+        while(j<m)
+        {
+            j++;z++;
+            if(z==k) return arr2[j-1];
+        }
+        return -1;
     }
 };
 
