3547b03b06a85b3f660e0a7aff17b37bc6972d1e
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-02-01 01:50:42 +0530

    Added solution - LeetHub

---
+++ Check whether BST contains Dead End - GFG/check-whether-bst-contains-dead-end.cpp
@@ -0,0 +1,127 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+        
+     cout<<isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+bool helper(Node *root, int l, long int r)
+
+{
+
+    if(!root)
+
+        return false;
+
+    if(root->data - l == 1 and r - root->data == 1)
+
+        return true;
+
+    return helper(root->left,l,root->data) or helper(root->right,root->data,r);
+
+}
+
+bool isDeadEnd(Node *root)
+
+{
+
+    return helper(root,0,INT_MAX);
+
+}
