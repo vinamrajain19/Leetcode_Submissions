515cafbf593a4a4defecee32d6d2aad58fbca586
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-02-02 00:34:05 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -1,34 +1,43 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
+    
+    static bool cmp(vector<int> &a,vector<int> &b){
+        if(a[1] == b[1]) return a[0] < b[0];
+        return a[1] < b[1];
+    }
     int maxMeetings(int start[], int end[], int n)
     {
-       vector<pair<int,int>> v;
-       for(int i=0;i<n;i++){
-           v.push_back({end[i],start[i]});
-       }
-       
-       sort(v.begin(),v.end());
-       int last=v[0].first;
-       int ans = 1;
-       for(int i=1;i<n;i++){
-           if(v[i].second > last){
-               ans++;
-               last=v[i].first;
-           }
-       }
-       return ans;
+        // Your code here
+        vector<vector<int>> v;
+        for(int i = 0;i<n;i++){
+            v.push_back({start[i],end[i]});
+        }
+        
+        sort(v.begin(),v.end(),cmp);
+        
+        int ans = 1;
+        int last = v[0][1];
+        
+        for(int i = 1;i<n;i++){
+            if(v[i][0] > last){
+                ans++;
+                last = v[i][1];
+            }
+        }
+        
+        return ans;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
@@ -45,4 +54,5 @@ int main() {
         cout << ans << endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
