b67ceeb58827ae4b3f60805b286fbef9698e3dca
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-02-12 02:39:20 +0530

    Added solution - LeetHub

--- Fractional Knapsack - GFG/fractional-knapsack.cpp
+++ Fractional Knapsack - GFG/fractional-knapsack.cpp
@@ -24,15 +24,17 @@ class Solution
     //Function to get the maximum total value in the knapsack.
     double fractionalKnapsack(int W, Item arr[], int n)
     {
+        // Your code here
         vector<pair<double,int>> v;
         for(int i = 0;i<n;i++){
             v.push_back({(double)arr[i].value/arr[i].weight,arr[i].weight});
+            
         }
         sort(v.begin(),v.end());
         double profit = 0.0;
         
         int i = n-1;
-        while(W > 0){
+        while(W){
             if(W >= v[i].second){
                 profit += v[i].first * v[i].second;
                 W -= v[i].second;
@@ -44,6 +46,7 @@ class Solution
             i--;
         }
         return profit;
+    
     }
         
 };
