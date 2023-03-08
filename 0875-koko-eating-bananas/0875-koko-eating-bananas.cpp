8c15c51e41eebff06ef49528446161b4bed25191
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-03-09 00:49:46 +0530

    Time: 74 ms (22.43%), Space: 18.9 MB (82.50%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& p, int h) {
+        int s = 1;
+        int e = *max_element(p.begin(),p.end());
+       
+
+        while(s<e){
+            int m = (s+e)/2;
+            
+            int x = 0;
+            for(int i= 0;i<p.size();i++){
+                x += (p[i]/m);
+                if(p[i]%m) x++;
+               }
+            if(x <= h){
+                e = m;
+                }
+            else s = m+1;
+            }
+        return s;
+                
+    }
+};
