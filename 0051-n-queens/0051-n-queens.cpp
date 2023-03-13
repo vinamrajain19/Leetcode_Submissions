8ae7ccd9767494824a2ed609b727eb24ad9937dc
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-03-13 17:48:28 +0530

    Time: 11 ms (42.87%), Space: 7.3 MB (67.14%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    
+    bool issafe(int i,int j,vector<string> &v){
+        
+        // upside
+        for(int k = 0;k<i;k++){
+            if(v[k][j] == 'Q') return false;
+        }
+        
+        //left diagonal
+        for(int k = i,m = j; k >=0 and m>=0; k--,m--){
+            if(v[k][m] == 'Q') return false;
+        }
+        
+        //right diagonal
+        for(int k = i,m = j; k >= 0 and m<v.size(); k--,m++){
+            if(v[k][m] == 'Q') return false;
+        }
+        
+        return true;
+    }
+    
+    void f(int n,vector<string> &v,int idx){
+        if(idx == n){
+            ans.push_back(v);
+            return;
+        }
+        
+        for(int j = 0;j<n;j++){
+            if(issafe(idx,j,v)){
+                v[idx][j] = 'Q';
+                f(n,v,idx+1);
+                v[idx][j] = '.'; //backtrack
+            }
+        }
+    }
+    
+    
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> v(n);
+        string s(n,'.');
+        
+        for(int i = 0;i<n;i++){
+            v[i] = s;
+        }
+        
+        f(n,v,0);
+        return ans;
+    }
+};
