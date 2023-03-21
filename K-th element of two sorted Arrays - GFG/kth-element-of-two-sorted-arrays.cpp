db08d8d04dbf7c1dc52b772177e9575c2a2bd090
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-03-21 21:09:30 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -6,38 +6,38 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        int i=0,j=0;
-        int z=0;
-        while(i<n && j<m)
-        {
-            if(arr1[i]<=arr2[j]) 
-            {
-                i++;z++;
-                if(z==k) return arr1[i-1];
-            }
-            else
-            {
-                j++;z++;
-                 if(z==k) return arr2[j-1];
-            }
-        }
-
-        while(i<n)
-        {
-            i++;z++;
-            if(z==k) return arr1[i-1];
-        }
-
-        while(j<m)
-        {
-            j++;z++;
-            if(z==k) return arr2[j-1];
-        }
-        return -1;
-    }
+       int i = 0;
+       int j = 0;
+       int z  = 0;
+       
+       while( i< n and j < m){
+           if(arr1[i] <= arr2[j]){
+               i++ ,z++;
+               if(z == k) return arr1[i-1];
+           }
+           else{
+               j++,z++;
+               if(z== k) return arr2[j-1];
+           }
+       }
+       
+       while(i<n){
+           i++;
+           z++;
+           if(z == k) return arr1[i-1];
+       }
+       
+       while(j<m){
+           j++;
+           z++;
+           if(z == k) return arr2[j-1];
+       }
+       
+       return -1;
+       
+    } 
 };
 
 //{ Driver Code Starts.
