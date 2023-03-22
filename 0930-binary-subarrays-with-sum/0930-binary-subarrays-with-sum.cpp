0cb65cb2ecabad3a5f12be79f489ca12fc688b62
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-03-22 18:19:16 +0530

    Time: 39 ms (71.91%), Space: 28.9 MB (77.46%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,21 +1,55 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int ans = 0;
-        int sum = 0;
-        unordered_map<int,int> m;
-        m[0] = 1;
+    
+    
+    int f(vector<int> &nums,int s){
+        if(s < 0) return 0;
         
-        for(int i = 0;i<nums.size();i++){
-            sum += nums[i];
+        int i = 0;
+        int j = 0;
+        
+        int sum = 0;
+        int ans = 0;
+        
+        while(j < nums.size()){
             
-            if(m.find(sum - goal) != m.end()){
-                ans += m[sum-goal];
+            sum += nums[j];
+            
+            while(sum > s and i < nums.size()){
+                sum -= nums[i];
+                i++;
             }
             
-            m[sum]++;
+            ans += j-i+1;
+            j++;
         }
-        
         return ans;
+    }
+    
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        
+         // S.C = O(N);
+        
+//         int ans = 0;
+//         int sum = 0;
+//         unordered_map<int,int> m;
+//         m[0] = 1;
+        
+//         for(int i = 0;i<nums.size();i++){
+//             sum += nums[i];
+            
+//             if(m.find(sum - goal) != m.end()){
+//                 ans += m[sum-goal];
+//             }
+            
+//             m[sum]++;
+//         }
+        
+//         return ans;
+        
+        
+        // S.C = O(1)
+        
+        return f(nums,goal) - f(nums,goal-1);
     }
 };
