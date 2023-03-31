7b8b90529efcfab9d272f1f57c18c6ea989c167e
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-03-31 17:08:31 +0530

    Time: 30 ms (45.67%), Space: 15.5 MB (12.68%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,60 +1,65 @@
 class Solution {
 public:
-    
-    string shortestCommonSupersequence(string text1, string text2) {
-       int m = text1.size();
-        int n = text2.size();
-        vector<vector<int>> t(m+1,vector<int>(n+1,0));
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
         
-        for(int i = 0;i<=m;i++){
-            t[i][0] = 0; 
-        }
-        for(int j = 0;j<=n;j++){
-            t[0][j] = 0;
-        }
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
         
         //main code
         for(int i = 1;i<m+1;i++){
             for(int j= 1;j<n+1;j++){
-                if(text1[i-1] == text2[j-1]){
-                    t[i][j] = 1 + t[i-1][j-1];
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
+
         }
         
+        int lcs = dp[m][n];
+        
+        int i = m;
+        int j = n;
         
         string ans = "";
-        int  i = m;
-        int j = n;
-        while(i>0 && j>0){
-            if(text1[i-1] == text2[j-1]){
-                ans = text1[i-1] + ans;
+        
+        while(i > 0  and j > 0){
+            if(str1[i-1] == str2[j-1]){
+                ans =  str1[i-1] + ans;
                 i--;
                 j--;
             }
-            // Take max from left and upper side and if it is left then take char from j else i
             else{
-                if(t[i-1][j] > t[i][j-1]){
-                    ans = text1[i-1]  + ans;
+                
+                if(dp[i-1][j] > dp[i][j-1]){
+                    ans = str1[i-1] + ans;
                     i--;
                 }
                 else{
-                    ans = text2[j-1] + ans;
+                    ans = str2[j-1] + ans;
                     j--;
                 }
+                
+            
             }
+            
         }
-        while(i>0){
-            ans = text1[i-1] + ans;
+        
+        while(i > 0){
+            ans = str1[i-1] + ans;
             i--;
         }
-        while(j>0){
-            ans = text2[j-1] + ans;
+        
+        while(j > 0){
+            ans = str2[j-1] + ans;
             j--;
         }
+        
         return ans;
+        
+        
     }
 };
