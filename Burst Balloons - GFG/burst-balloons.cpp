15bc2131b4e613a5151c35b640abb142181bd8ca
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-04-02 18:14:54 +0530

    Added solution - LeetHub

---
+++ Burst Balloons - GFG/burst-balloons.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+public:
+
+    int dp[305][305];
+    
+    int f(vector<int> &nums,int i,int j){
+        if(i >= j) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int ans = -1e9;
+        for(int k = i;k<j;k++){
+            
+            int temp = (nums[i-1] * nums[k] * nums[j]) + f(nums,i,k) + f(nums,k+1,j);
+            ans = max(ans,temp);
+        }
+        
+        return dp[i][j] =  ans;
+    }
+    
+    int maxcoins(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        nums.push_back(1);
+        nums.insert(nums.begin(),1);
+        // int n = nums.size()-1;
+        
+        return f(nums,1,nums.size()-1);
+    }
+    
+    int maxCoins(int N, vector<int> &arr) {
+        // code here
+        return maxcoins(arr);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        vector<int> arr(N);
+        for(int i=0; i<N; i++)
+            cin>>arr[i];
+
+        Solution obj;
+        cout << obj.maxCoins(N, arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
