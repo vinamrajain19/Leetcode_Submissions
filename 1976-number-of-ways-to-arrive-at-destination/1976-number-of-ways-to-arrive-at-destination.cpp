7e7fe45f4b75c93295385c9c17873b9bd5e8e53b
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-04-11 20:29:47 +0530

    Time: 83 ms (81.71%), Space: 30.3 MB (92.40%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
-    int mod = 1000000007;
+    
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<long>ways(n, 0);
-        vector<long >dis(n, LONG_MAX);
-        dis[0] = 0;
-        ways[0] = 1;
-		
-		//{distance, node}
-        priority_queue<pair<long ,long >, vector<pair<long ,long >>, greater<pair<long ,long >>>pq;
-        pq.push({0,0});
-
-        //creating the graph
-        //{node, distance}
-        vector<pair<long ,long >>adj[n];
+        int mod = 1e9+7;
         
-        for(auto r: roads){
-            adj[r[0]].push_back({r[1], r[2]});
-            adj[r[1]].push_back({r[0], r[2]});
+        vector<vector<pair<int, int>>> graph(n);
+        for(auto &road: roads) {
+            graph[road[0]].push_back({road[1], road[2]});
+            graph[road[1]].push_back({road[0], road[2]});
         }
         
-        while(pq.empty()==false){
-            long  currnode = pq.top().second;
-            long  currdis = pq.top().first;
+        vector<long long> distance(n, LONG_MAX);
+        vector<int> path(n, 0);
+        
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        pq.push({0, 0});
+        distance[0] = 0;
+        path[0] = 1;
+        
+        while(!pq.empty()) {
+            pair<long long, int> t = pq.top();
             pq.pop();
             
-            for(auto x: adj[currnode]){
-                long no = x.first;
-                long dist = x.second;
-                //if we are reaching a node with a distance lower than the current available distance then we update the distance of the current node and also put the no of ways to reach the currnode into the adjacent node ways, because we can reach the adj node same no of time as no of times we were able to reach currnode.
-                if(currdis+dist<dis[no]){
-                    dis[no] = currdis+dist;
-                    ways[no] = ways[currnode];
-                    pq.push({dis[no], no});
-                }
+            for(auto &nbr: graph[t.second]) {
+                long long vert = nbr.first;
+                long long edge = nbr.second;
                 
-				//if again we are able to reach a node with the same distance as available then we add the currnode ways into the adj node because now we can reach the adj node with the same smaller distance from some other paths as well. 
-                else if(currdis+dist == dis[no]){
-                    ways[no] = (ways[no]+ways[currnode])%(mod);
+                if(distance[vert] > distance[t.second] + edge) {
+                    distance[vert] = distance[t.second] + edge;
+                    pq.push({distance[vert], vert});
+                    path[vert] = path[t.second] %mod;
+                }
+                else if(distance[vert] == t.first + edge) {
+                    path[vert] += path[t.second];
+                    path[vert] %= mod;
                 }
             }
         }
         
-        return ways[n-1];
+        return path[n-1];
     }
+    
 };
