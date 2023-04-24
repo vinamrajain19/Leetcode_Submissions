624e05948962db4c5c50ee21c34669a2c016d6a6
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-04-24 21:19:14 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> q(stones.begin(),stones.end());
+        
+        while(q.size()>1){
+         
+            int max1 = q.top();
+            q.pop();
+            int max2 = q.top();
+            q.pop();
+            if(max1 != max2) {           
+                q.push(max1-max2);
+            }
+        }
+        return q.size() == 0 ? 0 : q.top();
+    }
+};
