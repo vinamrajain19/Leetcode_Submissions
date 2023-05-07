73e87c541856bd9240e4ed072b6eea0f8cfd82e7
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-05-07 12:15:02 +0530

    Time: 101 ms (29.50%), Space: 30.4 MB (66.77%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
-    
-    // BFS
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
-        vector<vector<int>> d = {{-1,0},{1,0},{0,-1},{0,1}};
-        
-        int m = mat.size();
-        int n = mat[0].size();
-        
-        vector<vector<int>> ans(m,vector<int>(n,1e9));
+         
+        int m = mat.size(), n = mat[0].size();
+		
+		// initializing result matrix with max value (distance)
+        vector<vector<int>> res(m, vector<int>(n,INT_MAX));
+		
+		// using queue to store position of elements whose answer we know, to use it to derive others' distances
         queue<pair<int,int>> q;
         
-        for(int i  = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(mat[i][j] == 0){
-                    ans[i][j] = 0;
+		// distance of 0 from nearest 0 will be 0 (itself)
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0){
+                    res[i][j] = 0;
                     q.push({i,j});
                 }
             }
         }
         
-        while(q.size()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
+		// right, bottom, left, top - directions in which we can move
+        vector<pair<int,int>> dirs{{0,1},{1,0},{0,-1},{-1,0}};
+        
+        while(!q.empty()){
+            auto cur = q.front(); q.pop();
+			// position of element whose result is known
+            int curx = cur.first;
+            int cury = cur.second;
             
-            for(auto it : d){
-                int ii = x + it[0];
-                int jj = y + it[1];
+			// traversing in all possible direction
+            for(auto dir: dirs){
+                int x = curx + dir.first;
+                int y = cury + dir.second;
                 
-                if(ii < 0 or jj< 0 or ii >= m or jj >= n or ans[ii][jj] != 1e9){
-                    continue;
+				// if the new position is valid
+                if(x>=0 and x<m and y>=0 and y<n){
+				
+				// update the distance if previous distance is more than distance of current element + 1 (bcoz. we can move only 1 step at a time)
+                    if(res[x][y] > res[curx][cury] + 1){
+                        res[x][y] = res[curx][cury] + 1;
+						// adding result to queue to use to derive other distances
+                        q.push({x,y});
+                    }
                 }
-                
-                 if(ans[ii][jj] > ans[x][y] + 1){
-                      ans[ii][jj]  = ans[x][y] + 1;
-                      q.push({ii,jj});
-                 }
             }
         }
         
-        
-        
-        return ans;
-        
+		// yayyyy now we have our result vector !!
+        return res;
     }
 };
