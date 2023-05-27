c304b7d1fcc30f4045d89e7dca8831748f70f230
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-05-27 16:39:13 +0530

    Time: 122 ms (17.49%), Space: 11.3 MB (55.19%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,71 +1,40 @@
 class Solution {
 public:
-    
-    int dp[2001][2001];
-    
-    
-    int f(string s,string p,int i,int j){
-        
-        if(i == 0 and j == 0) return true; 
-        
-        if(i > 0 and j == 0) return false;
-        
-        if(i == 0 and  j > 0){
-            while(j > 0){
-                if(p[j-1] != '*') return false;
-                j--;
-            }
-            return true;
-        }
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        
-        if(s[i-1] == p[j-1] or p[j-1] == '?') return dp[i][j] =  f(s,p,i-1,j-1);
-        
-        if(p[j-1] == '*'){
-            return dp[i][j] = f(s,p,i-1,j) or f(s,p,i,j-1);
-        }
-        
-        return dp[i][j] =  false;
-       
-    }
-    
     bool isMatch(string s, string p) {
-        vector<bool> prev(p.size()+1,0);
-        vector<bool> curr(p.size()+1,0);
+         int n = s.size();
+        int m = p.size();
         
-        prev[0] = 1;
+        vector < vector < bool >>  dp(n+1,vector<bool>(m+1,false));
         
-       // for(int i = 1;i<=s.size();i++) prev[i] = 0;
+        dp[0][0] = true;
         
-        for(int j = 1;j<=p.size();j++){
-            if(p[j-1] == '*')
-                prev[j] = prev[j-1];
+        for(int i = 1;i<=n;i++){
+            dp[i][0] = false;
         }
         
+        for(int j = 1;j<=m;j++){
+            if(p[j-1] == '*')
+                dp[0][j] = dp[0][j-1];
+        }
         
-        for(int i = 1;i<=s.size();i++){
-            for(int j = 1;j<=p.size();j++){
+        for(int i = 1;i<=n;i++){
+            
+            for(int j = 1;j<=m;j++){
                 
-                if(s[i-1] == p[j-1] or p[j-1] == '?'){
-                    curr[j] = prev[j-1];
+                if(p[j-1] == s[i-1] or p[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
                 }
                 
                 else if(p[j-1] == '*'){
-                    curr[j] = prev[j] or curr[j-1];
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 }
                 else{
-                    curr[j] = 0;
-                }
+                    dp[i][j] = false;
+                } 
+                
             }
-            
-            prev = curr;
         }
         
-        
-        return prev[p.size()];
-        
-        
+        return dp[n][m];
     }
 };
