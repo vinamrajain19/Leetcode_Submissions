a29ca11c7f75f908195b5e41285a75252d7c06fe
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-05-27 16:27:51 +0530

    Time: 263 ms (63.84%), Space: 90 MB (62.29%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     
     int dp[100001][3][3];
-    int f(vector<int> &prices,int idx,int buy,int cnt){
-
-        if(idx >= prices.size()) return 0;
-        if(cnt == 2) return 0;
-
-        if(dp[idx][buy][cnt] != -1) return dp[idx][buy][cnt];
-
-        int a = 0;
-        int b = 0;
-        if(buy){
-            a = -prices[idx] + f(prices,idx+1,0,cnt);
-            b = f(prices,idx+1,1,cnt);
-        }
-        else{
-            a = prices[idx] + f(prices,idx+1,1,cnt+1);
-            b = f(prices,idx+1,0,cnt);
-        }
-
-        return dp[idx][buy][cnt] = max(a,b);
+int f(const vector<int> &A,int idx,int buy,int cnt){
+    
+    if(cnt == 2) return 0;
+    
+    if(idx >= A.size()) return 0;
+    
+    if(dp[idx][buy][cnt] != -1) return dp[idx][buy][cnt];
+    
+    
+    if(buy){
+        return dp[idx][buy][cnt] =  max(-A[idx] + f(A,idx+1,0,cnt),f(A,idx+1,1,cnt));
     }
+    else return dp[idx][buy][cnt] = max(A[idx] + f(A,idx+1,1,cnt+1),f(A,idx+1,0,cnt));
+}
     int maxProfit(vector<int>& prices) {
         memset(dp,-1,sizeof(dp));
         return f(prices,0,1,0);
