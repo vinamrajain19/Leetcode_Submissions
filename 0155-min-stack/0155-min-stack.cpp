f370c7a7a0f114712e8c79cb0adfe39335053532
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-05-31 20:14:39 +0530

    Time: 23 ms (71.01%), Space: 16.3 MB (77.61%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,34 +1,28 @@
 class MinStack {
 public:
     stack<int> s;
-    int mn;
-    MinStack() {
-        mn = INT_MAX;
-    }
+    int mini = INT_MAX;
     
     void push(int val) {
-        if(val <= mn){
-            s.push(mn);
-            mn = val;
+        if (val <= mini) //we'll push previous min value into the stack before pushing new min val, coz when we'll pop eg. min element then we need to update mini with prev min value   
+        {
+            s.push(mini); //we'll use it to update mini when popping min element!
+            mini = val; //update mini with min val
         }
         s.push(val);
     }
     
     void pop() {
-        if(s.top() == mn){
-            s.pop();
-            mn = s.top();
+        if (s.top() == mini) //this means mini got updated here, and we know that we have stored previous min value in stack before pushing new minimum!
+        {
+            s.pop(); //popping curr min value 
+            mini = s.top(); //this is our previous min value, we'll update mini, and now we'll pop it as well!
         }
         s.pop();
     }
     
-    int top() {
-        return s.top();
-    }
-    
-    int getMin() {
-        return mn;
-    }
+    int top() {return s.top(); } 
+    int getMin() { return mini; }
 };
 
 /**
