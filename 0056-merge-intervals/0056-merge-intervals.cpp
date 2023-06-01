140c7f94cdbbefaf2688d2538148d251924e81d4
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-06-01 20:14:01 +0530

    Time: 38 ms (81.16%), Space: 19 MB (59.63%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    
-    static bool cmp(vector<int> &a,vector<int> &b){
-        if(a[0] == b[0]) return a[1] < b[1];
-        return a[0] < b[0];
-    }
-    
-    vector<vector<int>> merge(vector<vector<int>>& in) {
+    vector<vector<int>> merge(vector<vector<int>>& it) {
+        sort(it.begin(),it.end());
         vector<vector<int>> ans;
+        vector<int> t = it[0];
         
-        sort(in.begin(),in.end(),cmp);
-        
-        vector<int> t = in[0];
-        
-        for(int i = 1;i<in.size();i++){
-            if(t[1] < in[i][0]){
+        for(int i = 1;i<it.size();i++){
+            
+            if(t[1] < it[i][0]){
                 ans.push_back(t);
-                t = in[i];
+                t = it[i];
             }
             else{
-                t[0] = min(t[0],in[i][0]);
-                t[1] = max(t[1],in[i][1]);
+                t[0] = min(t[0],it[i][0]);
+                t[1] = max(t[1],it[i][1]);
             }
-            
-            
-        }   
+        }
         
         ans.push_back(t);
         return ans;
