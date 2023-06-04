b6ec17fb918c31c28d0e0161aac13610cbdfec86
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-06-04 15:09:13 +0530

    Time: 7 ms (26.12%), Space: 7.6 MB (90.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     
-    
     int dp[101][101];
-    int f(vector<vector<int>>& o,int i,int j){
-        int m = o.size(),n=o[0].size();
+    int f(vector<vector<int>> &o,int i,int j){
+        if(i >= o.size() or j>=o[0].size() or o[i][j] == 1) return 0;
         
-        if(i >= m or j>= n) return 0;
-        
-        if(o[i][j] == 1) return 0;
-        
-        if(i == m-1 and j == n-1) return 1;
+        if(i == o.size()-1 and j == o[0].size()-1) return 1;
         
         if(dp[i][j] != -1) return dp[i][j];
-        
-        
         
         int a = f(o,i+1,j);
         int b = f(o,i,j+1);
         
-        return dp[i][j] =  a+b;
-        
+        return dp[i][j] =  a + b;
     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& o) {
