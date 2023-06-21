001f85c269f7f0d989a71594f8a8b0892461dc7a
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-06-22 01:56:06 +0530

    Time: 8 ms (34.08%), Space: 14.2 MB (32.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,16 +1,26 @@
 class Solution {
-public:
-    vector<string>ans;
-    vector<string>generateParenthesis(int n) {
-        f("",n,n);
-        return ans;
-    }
-    void f(string s, int open, int close){
-        if(open==0 && close==0){
-            ans.push_back(s);
+private:
+    void f(string digits, vector<string>& output, string &temp, vector<string>& pad, int index){
+        if(index == digits.size()){
+            output.push_back(temp);
             return;
         }
-        if(open>0)f(s+"(",open-1,close);
-        if(open<close)f(s+")",open,close-1);
+        string value = pad[digits[index]-'0'];
+        for(int i=0; i<value.size(); i++){
+            temp.push_back(value[i]);
+            f(digits, output, temp, pad, index+1);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()){
+            return {};
+        }
+        vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> output;
+        string temp;
+        f(digits, output, temp, pad, 0);
+        return output;
     }
 };
