2406b03ac90df31080a7f9785d30d056a88639e1
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-06-22 01:27:48 +0530

    Time: 4 ms (36.19%), Space: 6.5 MB (59.77%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    void f(string digits, vector<string>& output, string &temp, vector<string>& pad, int index){
+        if(index == digits.size()){
+            output.push_back(temp);
+            return;
+        }
+        string value = pad[digits[index]-'0'];
+        for(int i=0; i<value.size(); i++){
+            temp.push_back(value[i]);
+            f(digits, output, temp, pad, index+1);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()){
+            return {};
+        }
+        vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> output;
+        string temp;
+        f(digits, output, temp, pad, 0);
+        return output;
+    }
+};
