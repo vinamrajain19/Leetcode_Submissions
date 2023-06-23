a9fbcda793197be96a38387c5fbc1283b36b50d7
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-06-23 11:48:36 +0530

    Time: 21 ms (27.19%), Space: 13.2 MB (51.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+        int m=matrix.size(), n=matrix[0].size();
         
-       for (int i = 0; i < rows; i++) {
-        //checking if 0 is present in the 0th column or not
-        if (matrix[i][0] == 0) col0 = 0;
-        for (int j = 1; j < cols; j++) {
-             if (matrix[i][j] == 0) {
-                 
-                 matrix[i][0] = 0;
-                 matrix[0][j] = 0;
+        bool isRow0=false, isCol0=false;
+        
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0)
+                isRow0=true;
+        }
+        
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0)
+                isCol0=true;
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
             }
         }
-    
-     }
         
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0)
+                    matrix[i][j]=0;
+            }
+        }
         
-  //traversing in the reverse direction and
-  //checking if the row or col has 0 or not
-  //and setting values of matrix accordingly.
-  for (int i = rows - 1; i >= 0; i--) {
-    for (int j = cols - 1; j >= 1; j--) {
-      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-        matrix[i][j] = 0;
-      }
+        if(isRow0){
+            for(int j=0;j<n;j++)
+                matrix[0][j]=0;
+        }
+        
+        if(isCol0){
+            for(int i=0;i<m;i++)
+                matrix[i][0]=0;
+        }
     }
-    if (col0 == 0) {
-      matrix[i][0] = 0;
-    }
-
-  }
-        
-}
 };
