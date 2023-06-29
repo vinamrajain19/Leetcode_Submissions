aa3e0b1e9d418aa650bebe987dd29ee0bded3733
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-06-29 11:13:07 +0530

    Time: 7 ms (61.68%), Space: 13.9 MB (91.76%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        if(nums.size() == 1) return nums[0] == target;
-        
+    bool search(vector<int>& nums, int t) {
         int s = 0;
         int e = nums.size()-1;
         
+        
         while(s <= e){
             
-            while(s+1 < nums.size() and nums[s] == nums[s+1]) s++;
-            while(e-1 >= 0 and nums[e] == nums[e-1]) e--;
-            
+             while (s < e && nums[s] == nums[s + 1])
+                ++s;
+             while (s < e && nums[e] == nums[e - 1])
+                --e;
             
             int m = (s+e)/2;
             
-            if(nums[m] == target) return true;
+            if(nums[m] == t) return true;
             
-            if(nums[m] >= nums[s]){
-                if(nums[s] <= target and nums[m] > target) e = m-1;
-                else s = m +1;
+            if(nums[s] <= nums[m]){
+                if(nums[s] <= t and nums[m] >= t){
+                    e = m -1;
+                }
+                else s = m+1;
             }
-            else {
-                if(nums[m] < target and nums[e] >= target) s = m+1;
+            else{
+                if(nums[m] <= t and nums[e] >= t) s = m +1;
                 else e = m-1;
             }
         }
-        return false;        
+        
+        return false;
     }
 };
