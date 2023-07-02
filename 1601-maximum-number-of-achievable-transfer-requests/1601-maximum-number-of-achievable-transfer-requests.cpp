02151f602eb703b30c43ed5350464f1a93e6ac42
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-02 22:31:06 +0530

    Time: 90 ms (90.20%), Space: 8.7 MB (75.49%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    void f(int i, vector<vector<int>>& requests,vector<int> &degree,int count,int &ans){
+        if(i==requests.size())
+        {
+            for(auto i:degree){
+                if(i!=0) return;
+            }
+            ans=max(ans,count);
+            return ;
+        }
+        //take
+        degree[requests[i][0]]++;
+        degree[requests[i][1]]--;
+        
+        f(i+1,requests,degree,count+1,ans);
+        
+        degree[requests[i][0]]--;
+        degree[requests[i][1]]++;
+        //not take
+        f(i+1,requests,degree,count,ans);
+
+    }
+    
+    
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        vector<int> degree(n);
+        int ans=0;
+        f(0,requests,degree,0,ans);
+        return ans;
+        
+    }
+};
