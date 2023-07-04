f3f176f50b5060078e7cdb21aac78a1129a302f1
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-04 11:52:41 +0530

    Time: 15 ms (62.83%), Space: 6 MB (85.53%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -4,38 +4,41 @@ class Solution
     
     
     //lz -> leading zeros-> we have leading zeroes if lz=1 else we have started with a non-zero digit
-    int dp[11][2][1<<10][2];
     
-    int f(int i,int tight,int mask,string &s,int lz){
-        if(i == s.size()) return 1;
+    int dp[1<<10][10][2][2];
+    
+    int f(string &s,int i,int mask,int lz,int tight)
+    {
+        if(i==s.length()) return 1;
         
-        if(dp[i][tight][mask][lz] != -1) return dp[i][tight][mask][lz];
+        if(dp[mask][i][lz][tight]!=-1)
+            return dp[mask][i][lz][tight];
+        
+        int ub = tight ? s[i]-'0': 9;
         
         int ans = 0;
-        int ub = tight ? s[i] - '0' : 9;
         
-        for(int dig = 0;dig<=ub;dig++){
+        for(int j=0;j<=ub;j++)
+        {
+            if((mask&(1<<j))&&lz==0) continue; // repeated no
             
-            if(lz == 1 and dig == 0) ans += f(i+1,0,mask,s,1);
+            mask|=(1<<j);                 // mask it
             
-            else if(mask & (1 << dig)) continue;
+            ans+=f(s,i+1,mask,lz&(j==0),tight&(j==ub));
             
-            else{
-                mask = mask | (1 << dig);
-                ans += f(i+1,(tight & (dig == ub)),mask,s,0);
-                mask = mask ^ (1 << dig);
-            } 
+            mask^=(1<<j); // unmask it
         }
         
-        return dp[i][tight][mask][lz] = ans;
+        return dp[mask][i][lz][tight]=ans;
     }
+    
   
     int numDupDigitsAtMostN(int n)
     {
         string s=to_string(n);
         memset(dp,-1,sizeof(dp));
         int mask=0;
-        int ans = (n+1)-f(0,1,mask,s,1);
+        int ans = (n+1)-f(s,0,mask,1,1);
         return ans;
     }
 };
