4327d4ef22926e06c37419e13ac97b6a190cd426
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-09 20:17:43 +0530

    Time: 12 ms (15.83%), Space: 12 MB (37.15%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     
-    int dp[5001][3];
-    int f(vector<int> &p,int idx,int buy){
-        if(idx >= p.size()) return 0;
-
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-
-        if(buy == 1){
-            return dp[idx][buy] = max(-p[idx] + f(p,idx+1,0), f(p,idx+1,1));
+    int dp[50001][3];
+    int f(vector<int> &p,int i,int buy){
+        if(i >= p.size()) return 0;
+        
+        if(dp[i][buy] != -1) return dp[i][buy];
+        
+        if(buy){
+            return dp[i][buy]  = max(-p[i] + f(p,i+1,0), f(p,i+1,1));
         }
-        else{
-            return dp[idx][buy]=  max(p[idx] + f(p,idx+2,1),f(p,idx+1,0));
-        }
+        else return dp[i][buy] =  max(p[i] + f(p,i+2,1),f(p,i+1,0));
     }
     int maxProfit(vector<int>& prices) {
         memset(dp,-1,sizeof(dp));
