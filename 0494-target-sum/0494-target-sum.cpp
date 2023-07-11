e45e7d524e7dde6bbc68e147227905c3c3c2baf8
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-11 23:33:15 +0530

    Time: 35 ms (48.05%), Space: 38.7 MB (18.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,46 +1,33 @@
 class Solution {
 public:
     
-    int mod = 1e9+7;
+    // int dp[22][2010];
+    vector<vector<int>> dp;
     
-    int f(vector<int> &arr,int n,int sum,int idx,vector<vector<int>> &dp){
-	   // if(sum == 0) return 1;
-	   
-	    if(idx == n-1){
-	        if(sum == 0 and arr[idx] == 0) return 2;
-	        if(sum == 0 or arr[idx]== sum) return 1;
-	        return 0;
-	    }
-	    
-	    if(dp[idx][sum] != -1) return dp[idx][sum];
-	    
-	    int a = 0;
-	    if(arr[idx] <= sum){
-	        a = f(arr,n,sum-arr[idx],idx+1,dp) % mod;
-	    }
-	    int b = f(arr,n,sum,idx+1,dp) % mod;
-	    
-	    return dp[idx][sum] =  (a + b) % mod;
-	}
-    
-    
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        int sum = accumulate(arr.begin(),arr.end(),0);
-        
-        int x = (sum - d)/2;
-        
-        if((sum-d) < 0) return 0;
-        
-        if((sum-d) % 2 == 1) return 0;
-        
-        vector<vector<int>> dp(n+1,vector<int>(x+1,-1));
-        
-        return f(arr, n,x,0,dp) % mod;
+    int helper(vector<int>& nums,int target,int n,int index,int sum){
+        if(index==n){
+            if(sum==target){
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[index][sum+1000]!=-1){
+            return dp[index][sum+1000];
+        }
+        return dp[index][sum+1000]= helper(nums,target,n,index+1,sum+nums[index]) + helper(nums,target,n,index+1,sum);
     }
     
-    
     int findTargetSumWays(vector<int>& nums, int target) {
-        return countPartitions(nums.size(),target,nums);
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        if((sum-target)%2!=0){
+            return 0;
+        }
+        int newTarget = (sum-target)/2;
+        dp.resize(22,vector<int>(2010,-1));
+        return helper(nums,newTarget,n,0,0);
     }
 };
