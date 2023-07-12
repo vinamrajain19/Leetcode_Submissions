836092f0ec8fcc88c4083d41ab36d84b9f95cd67
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-12 15:40:48 +0530

    Time: 35 ms (50.67%), Space: 13.8 MB (71.40%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     
-    int dp[301][5002];
+    int dp[301][5001];
     
-    int f(vector<int> &c,int amt,int idx){
+    int f(int i,vector<int> &c,int amt){
         if(amt < 0) return 0;
+        if(amt == 0) return 1;
+        if(i>=c.size()) return 0;
         
-        if(idx == c.size()-1){
-            if(amt % c[idx] == 0){
-                return 1;
-            }
-            return 0;
-        }
+        if(dp[i][amt] != -1) return dp[i][amt];
         
-        if(dp[idx][amt] != -1) return dp[idx][amt];
-        
-        int a = 0;
-        
-        if(c[idx] <= amt){
-            a = f(c,amt-c[idx],idx);
-        }
-        
-        int b = f(c,amt,idx+1);
-        
-        return dp[idx][amt] =  a + b;
+        int a = f(i,c,amt-c[i]);
+        int b = f(i+1,c,amt);
+        return dp[i][amt] = a+b;
     }
+    
     int change(int amount, vector<int>& coins) {
         memset(dp,-1,sizeof(dp));
-        return f(coins,amount,0);
+        return f(0,coins,amount);
     }
 };
