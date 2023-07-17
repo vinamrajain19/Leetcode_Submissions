7ddb60cfe3a3f1b859d0af54acbabd1d71e324a0
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-17 13:47:51 +0530

    Time: 30 ms (43.31%), Space: 13 MB (89.45%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,35 @@
 typedef tuple<int,int,int> ti;
-
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        
+     
         vector<pair<int,int>> adj[n];
-        
-        for(auto it : flights){
-            adj[it[0]].push_back({it[1],it[2]});
+        for(int i = 0;i<flights.size();i++){
+            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
         }
         
+        
         vector<int> dis(n,1e9);
-        
         dis[src] = 0;
-        
         queue<ti> pq;
         pq.push({0,src,0});
         
-        while(pq.size()){
-            auto [d,node,steps] = pq.front();
+        while(pq.size() != 0){
+            auto [x,d,steps] = pq.front();
             pq.pop();
             
             
-            for(auto it : adj[node]){
-                if(dis[it.first] > d + it.second and steps <= k){
-                    dis[it.first] = d + it.second;
+            for(auto it : adj[d]){
+                if(dis[it.first] > x + it.second and steps <= k) {
+                    dis[it.first] = x + it.second;
                     pq.push({dis[it.first],it.first,steps+1});
                 }
             }
         }
         
-        if(dis[dst] >= 1e9) return -1;
+        if(dis[dst] >= 1e9) return -1;      
+        
         return dis[dst];
+        
     }
 };
