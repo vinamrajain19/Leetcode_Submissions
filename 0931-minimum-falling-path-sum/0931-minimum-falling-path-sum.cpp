dc7422d27a9c4b8bfccd7730dfa92c1f40d47584
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-21 11:41:01 +0530

    Time: 17 ms (65.51%), Space: 10.5 MB (54.47%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    
-    int dp[101][101];
-    
-    int f(vector<vector<int>> &mat,int i,int j){
-        
-        if(j < 0 or i >= mat.size() or j >= mat[0].size()) return 1e5;
-        
-        if(i == mat.size()-1) return mat[i][j];
-        
-        if(dp[i][j] != -1) return dp[i][j];
-         
-        int a = mat[i][j] + f(mat,i+1,j);
-        int b = mat[i][j] + f(mat,i+1,j-1);
-        int c = mat[i][j] + f(mat,i+1,j+1);
-        
-        return dp[i][j] =  min({a,b,c});
+    int solve(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>&dp) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        if (i > m-1 || j > n-1 || i < 0 || j < 0) return 1e9;
+        if (i == m-1) return matrix[i][j];
+
+        if (dp[i][j] != 1e9) return dp[i][j];
+                
+        int bottom = matrix[i][j] + solve(matrix, i + 1, j, dp);
+        int diagLeft = matrix[i][j] + solve(matrix, i + 1, j - 1, dp);
+        int diagRight = matrix[i][j] + solve(matrix, i + 1, j + 1, dp);
+
+        return dp[i][j] = min (bottom, min(diagLeft, diagRight));
     }
-    
-    
-    int minFallingPathSum(vector<vector<int>>& mat) {
-        
-        memset(dp,-1,sizeof(dp));
-        int ans = 1e9;
-        
-        for(int i = 0;i<mat[0].size();i++){
-            ans = min(ans,f(mat,0,i));
+
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>>dp(m, vector<int>(n, 1e9));
+
+        int mini = INT_MAX;
+        for (int x = 0; x < n; x++){
+            mini = min (mini, solve(matrix, 0, x, dp));
         }
-        
-        return ans;
+        return mini;
     }
 };
