689757de8958d5449198c5c36a7600f177ef3253
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-23 17:20:42 +0530

    Time: 124 ms (82.24%), Space: 13.2 MB (88.84%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
-    
-    
-    //int mx = -1e9;
-    
-    bool f(string &s1,string &s2){
+    bool comapre(string &s1,string &s2){
+        
         if(s1.size() != s2.size() + 1) return false;
         
         int i = 0;
         int j = 0;
         
-        while(i < s1.size()){
-            if(j < s2.size() and s1[i] == s2[j]){
+        while(i<s1.size()){
+            
+            if(j<s2.size() and s1[i] == s2[j]){
                 i++;
                 j++;
             }
-            else{
-                i++;
-            }
+            else i++;
         }
         
         if(i == s1.size() and j == s2.size()) return true;
-        
-       return false;
+        return false;
     }
     
-    static bool cmp(string &a,string &b){
-        return a.size() < b.size();
+    static bool comp(string &s1,string &s2){
+        return s1.size() < s2.size();
     }
-    
-    
     
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(),cmp);
         
-        vector<int> dp(words.size(),1);
+        int len = INT_MIN;
+        sort(words.begin(),words.end(),comp);
         
-        int ans = -1;
-        
-        for(int i = 0 ;i<words.size();i++){
-            
+        int n = words.size();
+        vector<int> dp(n,1);
+        for(int i = 0;i<n;i++){
             for(int j = 0;j<i;j++){
-                
-                if(f(words[i],words[j]) and dp[i] < 1 + dp[j]){
+                if(comapre(words[i],words[j]) && dp[j] + 1 > dp[i]){
                     dp[i] = 1 + dp[j];
                 }
-                
-            }            
-            ans = max(ans,dp[i]);
+            }
+            len = max(len,dp[i]);
         }
-        
-        return ans;
+        return len;
     }
 };
