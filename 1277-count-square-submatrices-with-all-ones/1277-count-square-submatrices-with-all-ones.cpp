a30222e075a806a36cd2e52c9edc3c3200c693d9
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-07-24 16:50:53 +0530

    Time: 75 ms (82.36%), Space: 26.2 MB (44.45%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -3,29 +3,35 @@ public:
     int countSquares(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        
-        
         vector<vector<int>> dp(m,vector<int>(n,0));
         
-        int ans = 0;
-        
         for(int i = 0;i<m;i++){
-            
-            for(int j = 0;j<n;j++){
+            dp[i][0] = mat[i][0];
+        }
+        for(int j = 0;j<n;j++){
+            dp[0][j] = mat[0][j];
+        }
+        
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
                 
-                if(i == 0 or j == 0){
-                    dp[i][j] = mat[i][j];
-                }
-                else if(mat[i][j]== 1){
-                    dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
-                }else{
-                    dp[i][j] = 0;
-                }
+                if(mat[i][j]){
+                    
+                    int u = dp[i-1][j];
+                    int l = dp[i][j-1];
+                    int ud = dp[i-1][j-1];
                 
-                ans += dp[i][j];
+                    dp[i][j] = 1 + min({u,l,ud});
+                }
             }
         }
         
-        return ans;
+        int cnt =  0;
+        for(int  i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                cnt += dp[i][j];
+            }
+        }
+        return cnt;
     }
 };
