e1d74f6acd4ce14c8d4d350b65738e03f08cd15c
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-08-04 17:35:59 +0530

    Time: 22 ms (67.08%), Space: 7.8 MB (77.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,58 +1,56 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        if(s == t) return s;
+        
+        int i = 0;
+        int j = 0;
+        
         unordered_map<char,int> m;
         
         for(auto it : t) m[it]++;
         
-        int i = 0;
-        int j = 0;
-        int ans = 1e9;
-        string x;
+       
         
+        int len = 1e9;
+        
+        int start  = -1;
         int cnt = m.size();
-        
-        int st = -1;
         
         while(j < s.size()){
             
+            
             if(m.find(s[j]) != m.end()){
                 m[s[j]]--;
-                if(m[s[j]] == 0){
-                    cnt--;
-                }
+                if(m[s[j]] == 0) cnt--;
             }
             
             
             
-            if(cnt > 0) j++;
-            
-            else if(cnt == 0){
+            while(cnt == 0){
                 
-                
-                while(cnt == 0 and i < s.size()){
-                    
-                    if(j-i+1 < ans){
-                        st = i;
-                        ans = j-i+1;
-                       // x = s.substr(i,ans);
-                    }
-                    
-                    if(m.find(s[i]) != m.end()){
-                        m[s[i]]++;
-                        if(m[s[i]] == 1) cnt++;
-                    }
-                    
-                    i++;
+                if(len > j-i+1){
+                    len = j-i+1;
+                    start = i;
                 }
                 
-                j++;
+                if(m.find(s[i]) != m.end()){
+                    m[s[i]]++;
+                    if(m[s[i]] == 1) cnt++;
+                }
                 
-                
+                i++;
             }
+            
+            j++;
         }
         
-        if(st == -1) return "";
-        return s.substr(st,ans);
+        cout<<start;
+        
+        if(len == 1e9 or start == -1) return "";
+        
+        return s.substr(start,len);
+        
+        
     }
 };
