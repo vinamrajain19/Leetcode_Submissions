3dc38aeb2b9441ddd58cb874255f09d316b3374c
Vinamra Jain <91343225+vinamrajain19@users.noreply.github.com>
2023-08-05 12:35:10 +0530

    Time: 1510 ms (16.88%), Space: 422.4 MB (13.46%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    
-    int dp[1001][1001];
-    int f(string s,string t,int i,int j){
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+    int help(int i,int j,string s,string t,vector<vector<unsigned int>> &dp){
+        
+        if(j<0) return 1;
+        if(i<0) return 0;
+        
         if(dp[i][j] != -1) return dp[i][j];
         
-        int a = 0;
-        
-        if(s[i] == t[j]){
-            a = f(s,t,i-1,j-1);
-        }
-        int b = f(s,t,i-1,j);
-        
-        return dp[i][j] = a+b;
+        if(s[i] == t[j]) return dp[i][j] = help(i-1,j-1,s,t,dp) + help(i-1,j,s,t,dp);
+        else return dp[i][j] =  help(i-1,j,s,t,dp);
     }
-    
     int numDistinct(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return f(s,t,s.size()-1,t.size()-1);
+        int n = s.size();
+        int m = t.size();
+        vector<vector<unsigned int>> dp(n,vector<unsigned int>(m,-1));
+        return help(n-1,m-1,s,t,dp);
     }
 };
